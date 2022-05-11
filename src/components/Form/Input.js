16e86028e5cd7e6e2bd7bdd5c81c8c0c54b71368
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-05-11 11:49:03 -0300

    input update

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -3,15 +3,19 @@ import styles from "./Input.module.css";
 
 const Input = ({ type, id, children, value, setValue, placeholder }) => {
   return (
-    <div className={styles.input}>
-      <label htmlFor={id}>{children}</label>
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={id}>
+        {children}
+      </label>
       <input
+        className={styles.input}
         type={type}
         placeholder={placeholder}
         id={id}
         value={value}
         onChange={({ target }) => setValue(target.value)}
       />
+      <p className={styles.error}>error</p>
     </div>
   );
 };
