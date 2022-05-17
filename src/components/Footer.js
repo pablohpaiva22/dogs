1cab33b411a0e8c8214d63d348b438385c544a05
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-05-17 18:48:38 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.img}>
+        <Dogs />
+      </div>
+
+      <p className={styles.text}>Dogs. Alguns direitos reservados</p>
+    </footer>
+  );
 };
 
 export default Footer;
