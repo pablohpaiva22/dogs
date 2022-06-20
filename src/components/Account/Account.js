56cb5fb6b481aab180a07d6112b47fc8ff279245
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-06-20 16:12:12 -0300

    menuMobile

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,16 +5,24 @@ import { ReactComponent as EstatisticaButton } from "../../Assets/estatisticas.s
 import { ReactComponent as PostarButton } from "../../Assets/adicionar.svg";
 import { ReactComponent as SairButton } from "../../Assets/sair.svg";
 import styles from "../Account/Account.module.css";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import { Routes, Route } from "react-router-dom";
 import AccountPost from "./AccountPost";
 import AccountStatistics from "./AccountStatistics";
 import AccountProfile from "./AccountProfile";
+import useMedia from "../../Hooks/useMedia";
 
 const Account = () => {
   const { logOut } = React.useContext(UserContext);
   const navigate = useNavigate();
+  const mobile = useMedia("(max-width: 40rem");
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   const handleClick = (e) => {
     logOut();
@@ -22,25 +30,42 @@ const Account = () => {
   };
 
   return (
-    <section className={`container ${styles.containerConta}`}>
+    <section className={`${styles.containerConta} container`}>
       <header className={styles.header}>
         <AccountTitle />
 
-        <nav className={styles.navigation}>
+        {mobile && (
+          <button
+            onClick={() => setMobileMenu(!mobileMenu)}
+            className={`${styles.hambBtn} ${
+              mobileMenu && styles.mobileButtonActive
+            }`}
+          ></button>
+        )}
+
+        <nav
+          className={`${mobile ? styles.navigationMobile : styles.navigation} ${
+            mobileMenu && styles.navMobileActive
+          }`}
+        >
           <NavLink to="/conta" end>
             <FeedButton />
+            {mobile && "Minhas Fotos"}
           </NavLink>
 
           <NavLink to="/conta/estatistica">
             <EstatisticaButton />
+            {mobile && "Estatísticas"}
           </NavLink>
 
           <NavLink to="/conta/postar">
             <PostarButton />
+            {mobile && "Adicionar Foto"}
           </NavLink>
 
           <NavLink onClick={handleClick} to="/">
             <SairButton />
+            {mobile && "Sair"}
           </NavLink>
         </nav>
       </header>
