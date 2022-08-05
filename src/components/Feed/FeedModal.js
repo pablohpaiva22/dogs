1a7bb7436ae8b0b674ea4f76d7fe58cbe6f3cdeb
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-08-05 18:34:57 -0300

    comment post

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -3,12 +3,18 @@ import { useRef } from "react";
 import { UserContext } from "../../UserContext";
 import styles from "./FeedModal.module.css";
 import useFetch from "../../Hooks/useFetch";
-import { PHOTO_GET } from "../../api";
+import { COMMENT_POST, PHOTO_GET } from "../../api";
 import Title from "../Utilitarios/Title";
+import useForm from "../../Hooks/useForm";
+import { Link } from "react-router-dom";
+import Enviar from "../../Assets/enviar.svg";
 
 const FeedModal = () => {
   const { data, loading, error, request } = useFetch();
-  const { setModal, photoId } = React.useContext(UserContext);
+  const [comment, setComment] = React.useState("");
+  const { setModal, photoId, login } = React.useContext(UserContext);
+  const logindata = React.useContext(UserContext);
+  const { value, onChange, setValue } = useForm();
   const modalRef = useRef();
 
   const handleClick = (e) => {
@@ -19,15 +25,34 @@ const FeedModal = () => {
     const { url, options } = PHOTO_GET(photoId);
 
     async function modalFetch() {
-      const fetchData = await request(url, options);
+      await request(url, options);
     }
 
     modalFetch();
   }, [photoId, request]);
 
+  React.useEffect(() => {
+    if (data) {
+      setComment(data.comments);
+    }
+  }, [data]);
+
+  async function handleCommentSubmit(e) {
+    e.preventDefault();
+
+    const { url, options } = COMMENT_POST(photoId, { comment: value });
+
+    await fetch(url, options);
+    setComment([
+      ...comment,
+      { comment_content: value, comment_author: logindata.data.nome },
+    ]);
+    setValue("");
+  }
+
   return (
     <section ref={modalRef} onClick={handleClick} className={styles.modal}>
-      {error && <p>Error.</p>}
+      {error && <p style={{ color: "white" }}>Error.</p>}
 
       {loading && <p style={{ color: "#fff" }}>Carregando...</p>}
 
@@ -41,35 +66,62 @@ const FeedModal = () => {
           </div>
 
           <div className={styles.info}>
-            <div className={styles.infoFirstInformation}>
-              <span>@{data.photo.author}</span>
-              <div>
-                <span className={`${styles.visualizacao}`}>
-                  {data.photo.acessos}
-                </span>
+            <div>
+              <div className={styles.infoFirstInformation}>
+                <Link
+                  className={styles.linkProfile}
+                  to={`/profile/${data.photo.author}`}
+                >
+                  @{data.photo.author}
+                </Link>
+                <div>
+                  <span className={`${styles.visualizacao}`}>
+                    {data.photo.acessos}
+                  </span>
+                </div>
               </div>
+
+              <Title>{data.photo.title}</Title>
+
+              <div className={styles.infoAnimals}>
+                <span>{`${data.photo.peso} Kg`}</span>
+                <span>{`${data.photo.idade} anos`}</span>
+              </div>
+
+              {comment && (
+                <div className={styles.comments}>
+                  {comment.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <span
+                          style={{ fontWeight: "bold" }}
+                        >{`${item.comment_author}: `}</span>
+                        <span>{item.comment_content}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
             </div>
 
-            <Title>{data.photo.title}</Title>
+            {login && (
+              <form
+                className={styles.commentsPOST}
+                onSubmit={handleCommentSubmit}
+              >
+                <textarea
+                  onChange={onChange}
+                  value={value}
+                  className={styles.commentBox}
+                  cols="30"
+                  rows="2"
+                  placeholder="Comente..."
+                ></textarea>
 
-            <div className={styles.infoAnimals}>
-              <span>{`${data.photo.peso} Kg`}</span>
-              <span>{`${data.photo.idade} anos`}</span>
-            </div>
-
-            {data.comments.length !== 0 && (
-              <div className={styles.comment}>
-                {data.comments.map((item) => {
-                  return (
-                    <div key={item.comment_ID}>
-                      <span
-                        style={{ fontWeight: "bold" }}
-                      >{`${item.comment_author}: `}</span>
-                      <span>{item.comment_content}</span>
-                    </div>
-                  );
-                })}
-              </div>
+                <button className={styles.btn}>
+                  <img src={Enviar} alt="Enviar Button" />
+                </button>
+              </form>
             )}
           </div>
         </div>
