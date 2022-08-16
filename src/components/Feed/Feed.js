e4cd853c8803aeb08d67efb11693db2b19e1ce0d
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-08-16 11:46:39 -0300

    infinite scroll

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -5,6 +5,35 @@ import { UserContext } from "../../UserContext";
 
 const Feed = () => {
   const { modal, setModal } = React.useContext(UserContext);
+  const [pages, setPages] = React.useState([0]);
+  const [inifite, setInfinite] = React.useState(true);
+
+  React.useEffect(() => {
+    let wait = false;
+
+    function infiniteScroll() {
+      if (inifite) {
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+
+        if (scroll > height * 0.9 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, [1000]);
+        }
+      }
+    }
+
+    window.addEventListener("scroll", infiniteScroll);
+    window.addEventListener("wheel", infiniteScroll);
+
+    return () => {
+      window.removeEventListener("scroll", infiniteScroll);
+      window.removeEventListener("wheel", infiniteScroll);
+    };
+  }, [inifite]);
 
   React.useEffect(() => {
     setModal(false);
@@ -12,7 +41,17 @@ const Feed = () => {
 
   return (
     <>
-      <FeedPhotos user={0} />
+      {pages.map((item, index) => {
+        return (
+          <FeedPhotos
+            key={index}
+            user={0}
+            pageNumber={item}
+            setInfinite={setInfinite}
+          />
+        );
+      })}
+
       {modal && <FeedModal />}
     </>
   );
--- src/components/Feed/FeedPhotos.js
+++ src/components/Feed/FeedPhotos.js
@@ -5,20 +5,25 @@ import FeedPhotosItem from "./FeedPhotosItem";
 import Loading from "../Utilitarios/Loading";
 import { PHOTOS_GET } from "../../api";
 
-const FeedPhotos = ({ user }) => {
+const FeedPhotos = ({ user, pageNumber, setInfinite }) => {
   const { data, loading, request, error } = useFetch();
 
   React.useEffect(() => {
     const getPhotos = async () => {
-      const { url, options } = PHOTOS_GET({ page: 0, total: 6, user });
-      await request(url, options);
+      const { url, options } = PHOTOS_GET({ page: pageNumber, total: 6, user });
+
+      const { response, json } = await request(url, options);
+
+      if (response && response.ok && json.length < 3) setInfinite(false);
     };
 
     getPhotos();
-  }, [request, user]);
+  }, [request, user, pageNumber, setInfinite]);
 
   if (error) return <p className="container">error</p>;
+
   if (loading) return <Loading />;
+
   if (data)
     return (
       <ul className={`container ${styles.photos}`}>
